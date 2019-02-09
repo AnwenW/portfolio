c9ec1ef649026fef55902ad63a06313cceda3be6
Anwen Williams <anwen@anwenwilliams.com>
2019-02-09 18:21:46 +0000

    page.php remove container div.page-title-text; restructure main.scss accordingly

--- page.php
+++ page.php
@@ -3,34 +3,18 @@
 	<div class="standard-page">
 
     <?php if(have_posts()) :
-  		while(have_posts()) : the_post(); ?>
+  		while(have_posts()) : the_post();
+		?>
 
 		<div class="page-title-text">
-			<h3 class="page-title"><?php the_title(); ?></h3>
+		<h3 class="page-title"><?php the_title(); ?></h3>
 		</div>
-		
-		<section class="page-content"><?php the_content(); ?></section>
 
-  		<?php endwhile;
-  	endif;
+		<div class="page-content"><?php the_content(); ?></div>
+
+  	<?php endwhile;
+  		endif;
   	?>
-
-      <!-- Advanced Custom Fields? What's this doing? -->
-  		<!-- <?php get_template_part('templates/loop')?>
-
-  		<?php if (is_page('Supplementary')) { the_field('basic'); } ?>
-
-  		<?php // set variable, use get to pull in data from appropriate field
-  			$data =  get_field('basic'); ?>
-
-  		<?php // if page is supplementary echo out content in html
-  			if (is_page('Supplementary')) {
-
-  				// echo '<div>' . the_field('basic') . '</div>';
-  				echo '<div class="test">' . $data . '</div>';
-  				echo '<div class="test">' . $data . '</div>';
-
-  		} ?> -->
 
 	</div> <!-- closes standard-page -->
 
